e64e145c9bbdd2c05aac5e71e9f049f74df93a30
Jai Pal <jaipal.saini.0895@gmail.com>
2024-06-27 20:29:56 +0530

    Mac factory method

--- AbstractFactory/factories/MacOSFactory.java
+++ AbstractFactory/factories/MacOSFactory.java
@@ -1,5 +1,16 @@
 package AbstractFactory.factories;
 
-public class MacOSFactory {
-    
+import AbstractFactory.buttons.Button;
+import AbstractFactory.buttons.MacOSButton;
+import AbstractFactory.checkboxes.Checkbox;
+import AbstractFactory.checkboxes.MacOSCheckbox;
+
+public class MacOSFactory implements GUIFactory {
+    public Button createButton() {
+        return new MacOSButton();
+    }
+
+    public Checkbox createCheckbox() {
+        return new MacOSCheckbox();
+    }
 }
